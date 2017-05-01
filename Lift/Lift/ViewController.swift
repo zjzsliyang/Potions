ec5956805b5cf445e1151dd7e91be50253f4830f
Yang Li <zjzsliyang@gmail.com>
2017-05-01 11:04:29 +0800

    docs(README): update README

---
+++ Lift/Lift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lift
+//
+//  Created by Yang Li on 24/04/2017.
+//  Copyright © 2017 Yang Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
