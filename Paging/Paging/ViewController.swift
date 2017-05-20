bed5e6fc127bafc1af4ba41692b5ab4a13117589
Yang Li <zjzsliyang@gmail.com>
2017-05-21 02:13:05 +0800

    feat(Paging): init fot dashboard

--- Paging/Paging/ViewController.swift
+++ Paging/Paging/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import Charts
+import TextFieldEffects
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+  @IBOutlet weak var dashBoard: UIView!
+  @IBOutlet weak var missNo: UILabel!
+  @IBOutlet weak var missRate: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    let memoryTextField = MadokaTextField(frame: CGRect(x: 250, y: 53, width: 170, height: 40))
+    memoryTextField.placeholder = "Physical Memory"
+    memoryTextField.placeholderFontScale = 1
+    memoryTextField.borderColor = .lightGray
+    memoryTextField.placeholderColor = .lightGray
+    let orderNoTextField = MadokaTextField(frame: CGRect(x: 250, y: 160, width: 170, height: 40))
+    orderNoTextField.placeholder = "No. of Order"
+    orderNoTextField.placeholderFontScale = 1
+    orderNoTextField.borderColor = .lightGray
+    orderNoTextField.placeholderColor = .lightGray
+    dashBoard.addSubview(memoryTextField)
+    dashBoard.addSubview(orderNoTextField)
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  @IBAction func algorithmChosen(_ sender: UISwitch) {
+    
   }
-
-
 }
-
---
+++ Paging/Pods/Target Support Files/Pods-Paging/Pods-Paging-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Paging : NSObject
+@end
+@implementation PodsDummy_Pods_Paging
+@end
