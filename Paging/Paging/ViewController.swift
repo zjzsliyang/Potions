b84e5aa64c08db8bb97b6f72f3e8ccc76f7923e0
Yang Li <zjzsliyang@gmail.com>
2017-05-21 03:11:50 +0800

    feat(Paging): add conclusionBoard

--- Paging/Paging/ViewController.swift
+++ Paging/Paging/ViewController.swift
@@ -10,29 +10,47 @@ import UIKit
 import Charts
 import TextFieldEffects
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, ChartViewDelegate {
   @IBOutlet weak var dashBoard: UIView!
+  @IBOutlet weak var conclusionBoard: UIView!
   @IBOutlet weak var missNo: UILabel!
   @IBOutlet weak var missRate: UILabel!
+  
+  var algorithm: Int = 1 // 1 fot LRU, 0 for FIFO
 
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let memoryTextField = MadokaTextField(frame: CGRect(x: 250, y: 53, width: 170, height: 40))
+    let memoryTextField = MadokaTextField(frame: CGRect(x: 720, y: 73, width: 170, height: 40))
     memoryTextField.placeholder = "Physical Memory"
     memoryTextField.placeholderFontScale = 1
     memoryTextField.borderColor = .lightGray
     memoryTextField.placeholderColor = .lightGray
-    let orderNoTextField = MadokaTextField(frame: CGRect(x: 250, y: 160, width: 170, height: 40))
+    let orderNoTextField = MadokaTextField(frame: CGRect(x: 720, y: 130, width: 170, height: 40))
     orderNoTextField.placeholder = "No. of Order"
     orderNoTextField.placeholderFontScale = 1
     orderNoTextField.borderColor = .lightGray
     orderNoTextField.placeholderColor = .lightGray
     dashBoard.addSubview(memoryTextField)
     dashBoard.addSubview(orderNoTextField)
+
   }
 
   @IBAction func algorithmChosen(_ sender: UISwitch) {
+    if sender.isOn {
+      algorithm = 1 // LRU Algorithm
+    } else {
+      algorithm = 0 // FIFO Algorithm
+    }
+  }
+  
+  @IBAction func step(_ sender: UIButton) {
     
   }
+  
+  @IBAction func stepForward(_ sender: UIButton) {
+    
+  }
+  
 }
